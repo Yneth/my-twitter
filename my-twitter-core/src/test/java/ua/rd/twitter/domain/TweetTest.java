097bf029761a0870e561bfca2968a72554622322
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-02 17:26:23 +0200

    Add tests

--- my-twitter-core/src/test/java/ua/rd/twitter/domain/TweetTest.java
+++ my-twitter-core/src/test/java/ua/rd/twitter/domain/TweetTest.java
@@ -11,6 +11,46 @@ import static org.junit.Assert.assertTrue;
 public class TweetTest extends AbstractEntityTest {
 
     @Test
+    public void testAddRetweetShouldModifyRetweetCollection() throws Exception {
+        Tweet tweet = new Tweet();
+        Retweet retweet = new Retweet();
+        tweet.addRetweet(retweet);
+
+        assertThat(tweet.getRetweets().size(), is(1));
+        assertTrue(tweet.getRetweets().contains(retweet));
+    }
+
+    @Test
+    public void getMentionedUserNames() throws Exception {
+
+    }
+
+    @Test
+    public void getReplyRecipient() throws Exception {
+
+    }
+
+    @Test
+    public void setOwner() throws Exception {
+
+    }
+
+    @Test
+    public void setMessage() throws Exception {
+
+    }
+
+    @Test
+    public void setLikes() throws Exception {
+
+    }
+
+    @Test
+    public void setRetweets() throws Exception {
+
+    }
+
+    @Test
     public void testLikeShouldBeAddedToTheLikesCollection() {
         Tweet tweet = new Tweet();
         Like like = LikeTestFactory.withIds(0L, 0L);
