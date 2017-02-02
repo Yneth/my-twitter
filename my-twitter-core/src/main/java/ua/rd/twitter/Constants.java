fe585ef66df742330a7e7c94da8fc758430575b3
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-02 17:08:20 +0200

    Add utility methods get mentions and reply from tweet

---
+++ my-twitter-core/src/main/java/ua/rd/twitter/Constants.java
@@ -0,0 +1,10 @@
+package ua.rd.twitter;
+
+import java.util.regex.Pattern;
+
+public final class Constants {
+    private Constants() { }
+
+    public static final String USERNAME_REGEX = "@\\w{1,15}";
+    public static final Pattern USERNAME_PATTERN = Pattern.compile(Constants.USERNAME_REGEX);
+}
--- my-twitter-core/src/main/java/ua/rd/twitter/domain/Tweet.java
+++ my-twitter-core/src/main/java/ua/rd/twitter/domain/Tweet.java
@@ -3,16 +3,20 @@ package ua.rd.twitter.domain;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import ua.rd.twitter.Constants;
 
 import java.util.*;
+import java.util.regex.Matcher;
 
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false, exclude = {"likes", "retweets"})
 public class Tweet extends AbstractEntity<Long> {
+    private static final String USERNAME_REGEX = "@\\w{1,15}";
+
     private User owner;
 
-    private String text;
+    private String message;
 
     private Set<Like> likes = new HashSet<>();
 
@@ -28,5 +32,26 @@ public class Tweet extends AbstractEntity<Long> {
 
     public void dislike(Like like) {
         likes.remove(like);
+    }
+
+    public List<String> getMentionedUserNames() {
+        List<String> result = new ArrayList<>();
+        if (message != null) {
+            Matcher matcher = Constants.USERNAME_PATTERN.matcher(message);
+            while (matcher.find()) {
+                if (matcher.start() != 0) {
+                    result.add(matcher.group());
+                }
+            }
+        }
+        return result;
+    }
+
+    public Optional<String> getReplyRecipient() {
+        Matcher matcher = Constants.USERNAME_PATTERN.matcher(message);
+        if (matcher.find() && matcher.start() == 0) {
+            Optional.of(matcher.group());
+        }
+        return Optional.empty();
     }
 }
