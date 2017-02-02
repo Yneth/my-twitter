041adb46e01ec924cd0c7561f4486960b4b8057a
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-02 17:25:48 +0200

    Move UserProfile tests to UserService

--- my-twitter-core/src/test/java/ua/rd/twitter/domain/UserProfileTest.java
+++ my-twitter-core/src/test/java/ua/rd/twitter/domain/UserProfileTest.java
@@ -1,59 +1,6 @@
 package ua.rd.twitter.domain;
 
-import org.junit.Test;
-import ua.rd.twitter.domain.exception.AlreadyFollowingException;
-import ua.rd.twitter.domain.exception.SelfFollowException;
-import ua.rd.twitter.domain.util.UserProfileTestFactory;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertTrue;
-
 public class UserProfileTest extends AbstractEntityTest {
-
-    @Test(expected = SelfFollowException.class)
-    public void testFollowYourself() {
-        UserProfile profile = UserProfileTestFactory.withId(1L);
-        profile.follow(profile);
-    }
-
-    @Test
-    public void testFollowShouldAddFollowingToTheCollection() {
-        UserProfile profile = UserProfileTestFactory.withId(1L);
-        UserProfile someOtherProfile = UserProfileTestFactory.withId(10L);
-        profile.follow(someOtherProfile);
-
-        assertThat(profile.getFollowing().size(), is(1));
-        assertTrue(profile.getFollowing().contains(someOtherProfile));
-    }
-
-    @Test(expected = AlreadyFollowingException.class)
-    public void testFollowAlreadyFollowingShouldThrow() {
-        UserProfile profile = UserProfileTestFactory.withId(1L);
-        UserProfile someOtherProfile = UserProfileTestFactory.withId(10L);
-        profile.follow(someOtherProfile);
-        profile.follow(someOtherProfile);
-    }
-
-    @Test
-    public void testFollowNonPersistentUserShouldNotBeFollowed() {
-        UserProfile toFollow = new UserProfile();
-        UserProfile follower = UserProfileTestFactory.withId(10L);
-        follower.follow(toFollow);
-
-        assertThat(toFollow.getFollowers().size(), is(0));
-        assertThat(follower.getFollowing().size(), is(0));
-    }
-
-    @Test
-    public void testNotPersistedUserFollowShouldDoNothing() {
-        UserProfile toFollow = UserProfileTestFactory.withId(10L);
-        UserProfile follower = new UserProfile();
-        follower.follow(toFollow);
-
-        assertThat(toFollow.getFollowers().size(), is(0));
-        assertThat(follower.getFollowing().size(), is(0));
-    }
 
     @Override
     protected AbstractEntity<Long> create() {
--- my-twitter-service/src/test/java/ua/rd/twitter/service/impl/UserServiceImplTest.java
+++ my-twitter-service/src/test/java/ua/rd/twitter/service/impl/UserServiceImplTest.java
@@ -1,5 +1,77 @@
 package ua.rd.twitter.service.impl;
 
-public class UserServiceImplTest {
+import org.junit.Before;
+import org.junit.Test;
+import ua.rd.twitter.domain.UserProfile;
+import ua.rd.twitter.domain.exception.AlreadyFollowingException;
+import ua.rd.twitter.domain.exception.SelfFollowException;
+import ua.rd.twitter.service.UserService;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class UserServiceImplTest {
+    private UserService userService;
+
+    @Before
+    public void setUp() {
+        userService = new UserServiceImpl();
+    }
+
+    @Test
+    public void follow() throws Exception {
+
+    }
+
+    @Test(expected = SelfFollowException.class)
+    public void testFollowYourself() {
+        UserProfile profile = createUserWithId(1L);
+        userService.follow(profile, profile);
+    }
+
+    @Test
+    public void testFollowShouldAddFollowingToTheCollection() {
+        UserProfile profile = createUserWithId(1L);
+        UserProfile someOtherProfile = createUserWithId(10L);
+        userService.follow(profile, someOtherProfile);
+
+        assertThat(profile.getFollowing().size(), is(1));
+        assertTrue(profile.getFollowing().contains(someOtherProfile));
+    }
+
+    @Test(expected = AlreadyFollowingException.class)
+    public void testFollowAlreadyFollowingShouldThrow() {
+        UserProfile profile = createUserWithId(1L);
+        UserProfile someOtherProfile = createUserWithId(10L);
+
+        userService.follow(profile, someOtherProfile);
+        userService.follow(profile, someOtherProfile);
+    }
+
+    @Test
+    public void testFollowNonPersistentUserShouldNotBeFollowed() {
+        UserProfile toFollow = new UserProfile();
+        UserProfile follower = createUserWithId(10L);
+        userService.follow(follower, toFollow);
+
+        assertThat(toFollow.getFollowers().size(), is(0));
+        assertThat(follower.getFollowing().size(), is(0));
+    }
+
+    @Test
+    public void testNotPersistedUserFollowShouldDoNothing() {
+        UserProfile toFollow = createUserWithId(10L);
+        UserProfile follower = new UserProfile();
+        userService.follow(follower, toFollow);
+
+        assertThat(toFollow.getFollowers().size(), is(0));
+        assertThat(follower.getFollowing().size(), is(0));
+    }
+
+    private UserProfile createUserWithId(Long id) {
+        UserProfile profile = new UserProfile();
+        profile.setId(id);
+        return profile;
+    }
 }
