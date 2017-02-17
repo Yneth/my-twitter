4c8eac6410835c9d5ff405f36462feb6000d94d5
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-17 15:26:11 +0200

    Replace factory function to constructor

--- my-twitter-core/src/main/java/ua/rd/twitter/domain/Retweet.java
+++ my-twitter-core/src/main/java/ua/rd/twitter/domain/Retweet.java
@@ -2,29 +2,25 @@ package ua.rd.twitter.domain;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Objects;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Retweet extends Tweet {
     private Tweet tweet;
 
-    public static Retweet from(Tweet tweet, User user) {
+    public Retweet(Tweet tweet, User user) {
         Objects.requireNonNull(tweet, "Tweet cannot be null.");
         Objects.requireNonNull(user, "User cannot be null.");
 
-        Retweet retweet = new Retweet();
-        retweet.setTweet(tweet);
-        retweet.setOwner(user);
+        this.setTweet(tweet);
+        this.setOwner(user);
 
-        tweet.getRetweets().add(retweet);
-        return retweet;
-    }
-
-    public static Retweet from(Retweet tweet, User user) {
-        return from(tweet.getTweet(), user);
+        tweet.getRetweets().add(this);
     }
 }
