107a351aa59d2d18a459cc3bbae00190e10bb70a
Anthony <zaknofein2@gmail.com>
2017-02-19 23:43:35 +0200

    Add recepient field to Reply

--- my-twitter-core/src/main/java/ua/rd/twitter/domain/Reply.java
+++ my-twitter-core/src/main/java/ua/rd/twitter/domain/Reply.java
@@ -3,24 +3,12 @@ package ua.rd.twitter.domain;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import ua.rd.twitter.Constants;
-
-import java.util.Optional;
-import java.util.regex.Matcher;
 
 @Getter
 @Setter
-@EqualsAndHashCode(callSuper = true, exclude = {"tweet",})
+@EqualsAndHashCode(callSuper = true, exclude = {"tweet", "recipient",})
 public class Reply extends Tweet {
     private Tweet tweet;
 
-    private String recipient;
-
-    public Optional<String> getRecipientUsername() {
-        Matcher matcher = Constants.USERNAME_PATTERN.matcher(message);
-        if (matcher.find() && matcher.start() == 0) {
-            return Optional.of(matcher.group());
-        }
-        return Optional.empty();
-    }
+    private UserProfile recipient;
 }
